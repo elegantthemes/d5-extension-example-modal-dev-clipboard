eb16429f33097de681e6d3a19f7ac31d4a83f50c
Fikri Rasyid <fikrirasyid@gmail.com>
2022-02-16 21:57:11 +0700

    feat(D5i, 3PS) :: Added event callback that will open registered modal (dev/clipboard) when admin bar item is clicked

--- includes/loader.js
+++ includes/loader.js
@@ -9,3 +9,17 @@ $(window).on('et_builder_api_ready', (event, API) => {
   API.registerModules(modules);
   API.registerModalFields(fields);
 });
+
+// Open divi/dev-clipboard, a registered modal, when clipboard menu item on admin bar is clicked.
+// This ensure that the following function is registered on app window.
+if (window.top !== window) {
+
+  // Listen to click event on top window's admin bar item. The condition above ensures that the
+  // following is only executed in app window.
+  window.top.jQuery('#wp-admin-bar-d5i-modal-dev-clipboard a').on('click', (event) => {
+    event.preventDefault();
+
+    // Open registered modal, divi/dev-clipboard.
+    window.wp.data.dispatch('divi/modals').open({ name: 'divi/dev-clipboard' });
+  });
+}
