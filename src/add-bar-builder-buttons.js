475417f5107ef5bc2347868b9f593ba1010b40f2
mohmed98 <mohamedatta447@gmail.com>
2025-02-05 12:31:42 +0200

    feat: Refactor button registration to use app-ui module for improved structure and readability

--- src/add-bar-builder-buttons.js
+++ src/add-bar-builder-buttons.js
@@ -1,28 +1,27 @@
+import { registerBuilderBarButton } from '@divi/app-ui';
 import {
   dispatch,
   select,
-  useSelect
 } from '@divi/data';
 
-window.divi.appUi.registerBuilderBarButton({
-    iconSvg: { name: 'dev-clipboard' },
-    label: 'My Second Button',
-    order: 20,
-    name: 'divi/dev-clipboard',
-    onClick: () => {
-      if(select('divi/modal-library').getModal('divi/dev-clipboard')?.isActive){
-        dispatch('divi/modal-library').close({
-          name: 'divi/dev-clipboard',
-        });
-      }else{
 
-        dispatch('divi/modal-library').open({
-          name: 'divi/dev-clipboard',
-        });
-      }
-
-    },
-  });
-
-
-
+registerBuilderBarButton({
+  iconSvg: { name: 'dev-clipboard' },
+  label:   'My Second Button',
+  order:   20,
+  name:    'divi/dev-clipboard',
+  onClick: () => {
+    // Check if the 'divi/dev-clipboard' modal is currently active
+    if (select('divi/modal-library').getModal('divi/dev-clipboard')?.isActive) {
+       // If the modal is active, close it
+      dispatch('divi/modal-library').close({
+        name: 'divi/dev-clipboard',
+      });
+    } else {
+      // If the modal is not active, open it
+      dispatch('divi/modal-library').open({
+        name: 'divi/dev-clipboard',
+      });
+    }
+  },
+});
