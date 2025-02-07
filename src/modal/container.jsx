b4ba3d09a7b3ae7d2dededb8bbe0790a82e1f039
mohmed98 <mohamedatta447@gmail.com>
2025-02-07 11:45:54 +0200

    fix: Add JSDoc comments to DevClipboardContainer for better documentation

--- src/modal/container.jsx
+++ src/modal/container.jsx
@@ -5,7 +5,16 @@ import {
 
 import { DevClipboard } from './component';
 
-
+/**
+ * Container component for the DevClipboard.
+ * 
+ * This component connects the DevClipboard component to the Divi clipboard store
+ * using the withSelect higher-order component.
+ * 
+ * @param {Function} withSelect - Higher-order component to connect to the Divi data store.
+ * @param {Function} selectStore - Function to select the Divi clipboard store.
+ * @returns {Object} - An object containing the clipboard items.
+ */
 export const DevClipboardContainer = withSelect(selectStore => {
   const clipboardItems = selectStore('divi/clipboard').getItems();
   return {
